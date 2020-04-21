d149939a9054f34c639055f123ca6e275c485867
TYF1 <43130968+TYF1@users.noreply.github.com>
2020-04-21 11:03:33 +0800

    modified:   functional_tests.py 	modified:   lists/templates/home.html 	modified:   lists/tests.py 	modified:   lists/views.py

--- functional_tests.py
+++ functional_tests.py
@@ -32,18 +32,28 @@ class NewVisitorTest(unittest.TestCase):
 
         table=self.browser.find_element_by_id('id_list_table')
         rows=table.find_elements_by_tag_name('tr')
-        self.assertTrue(any(row.text=='1:Buy peacock feathers' for row in rows),"New to-do item  did not appear in table")
+        self.assertIn('1:Buy peacock feathers',[row.text for row in rows])
         #There is still a text box inviting her to add another item.She
         #enters "Use peacock feathers to make a fly"(Edith is very methodical)
+        inputbox=self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Use peacock feather to make a fly')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+        #The page updates again, and now shows both itms on her list
+        table=self.browser.find_element_by_id('id_list_table')
+        rows=table.find_elements_by_tag_name('tr')
+        self.assertIn('1:Buy peacock feathers',[row.text for row in rows])
+        self.assertIn(
+            '2:Use peacock feathers to make fly',
+            [row.text for row in rows]
+        )
+        #Edith wonders whether the site will remrember her list.Then she sees
+        #that the site has generated a unique URL for her -- there is some
+        #explanatory text to that effect.
+
+        #She visitsthat URL - her to-do list isstill there.
+
+        #Satisfied ,she goes back to sleep
         self.fail('Finish the test')
 if __name__ == '__main__':
     unittest.main(warnings='ignore')
-#The page updates again, and now shows both itms on her list
-
-#Edith wonders whether the site will remrember her list.Then she sees
-#that the site has generated a unique URL for her -- there is some
-#explanatory text to that effect.
-
-#She visitsthat URL - her to-do list isstill there.
-
-#Satisfied ,she goes back to sleep
