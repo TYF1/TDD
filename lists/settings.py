786fa9b449b6be776625bf1f4c6eac1b90c21599
TYF1 <43130968+TYF1@users.noreply.github.com>
2020-04-28 11:31:58 +0800

    Redirect after POST,and show all items in templates

--- lists/settings.py
+++ lists/settings.py
@@ -7,3 +7,9 @@ INSTALLED_APPS=[
     'django.contrib.staticfiles',
     'lists',
 ]
+DATABASES={
+    'default':{
+        'ENGINE':'django.db.backends.sqlite3',
+        'NAME':os.path.join(BASE_DIR,'db.sqlite3'),
+    }
+}
