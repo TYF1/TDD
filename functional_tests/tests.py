c53455bfd9521c832fc2f4144eabf043d91b703b
TYF1 <43130968+TYF1@users.noreply.github.com>
2020-06-01 22:15:42 +0800

    Hack FT runner to be able to test staging

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -3,10 +3,15 @@ from selenium.common.exceptions import WebDriverException
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium.webdriver.common.keys import Keys
 import time
+import os
 MAX_WAIT=10
 class NewVisitorTest(StaticLiveServerTestCase):
     def setUp(self):
         self.browser=webdriver.Firefox()
+        staging_sever=os.environ.get('STAGING_SERVER')
+        if staging_sever:
+            self.live_server_url='http://'+staging_sever
+
         
     def tearDown(self):
         self.browser.refresh() 
